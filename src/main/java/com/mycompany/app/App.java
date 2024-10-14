0510247c9750377ca2a785c7339c79bf0e2b3540
Bowman Zhu <15365687987.com>
2024-10-14 18:25:32 +0800

    1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,4 +4,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+    // 添加 getMessage 方法
+    public String getMessage() {
+        return "Hello, World!";
+    }
 }
